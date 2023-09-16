5dcd48f75ef35683fa84dff2b4d2ecc01cb7ccbb
dont touch my mac <vishal@vishals-MacBook-Pro.local>
2023-09-16 22:39:51 +0530

    commit vlog page

--- src/pages/SinglePageBlog.jsx
+++ src/pages/SinglePageBlog.jsx
@@ -69,7 +69,9 @@ const SinglePageBlog = () => {
 
                  <div className='aurthor'>
                     <img src={firstImg} alt="" />
-
+                    <h3>About The Author</h3>
+                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+                     <button>Read More</button>
                  </div>
                  
             </div>
